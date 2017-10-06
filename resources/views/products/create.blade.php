0d965e390869cc6147245b7023fca5b8fbdb7040
kosgeivinny <vinnykosgei@gmail.com>
2017-10-06 14:06:01 +0300

    Products table created with form

---
+++ resources/views/products/create.blade.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vinny
+ * Date: 2017/10/06
+ * Time: 1:34 PM
+ */
+?>
+
+        <!DOCTYPE html>
+<head>
+    <title> Create Products  </title>
+</head>
+<body>
+<div class="container">
+<form>
+    <h2> Create a Product </h2>
+
+    <div class="row">
+    <label> Name: </label>
+    <input type="text">
+    </div>
+    <br/>
+    <br/>
+    
+    <label> Price: </label>
+    <input type="int">
+    <br/>
+    <br/>
+
+    <button type="submit"> Submit </button>
+
+</form>
+</div>
+</body>
+
